14a480a75e810e550b07bf6898202243378236f1
Desire Gardner <desiregardner@Desires-MacBook-Pro.local>
2012-03-05 00:57:38 -0600

    Added first files to the reo.

---
+++ main.js
@@ -0,0 +1,39 @@
+// Hello World!
+//Desire Revon Gardner
+//Created for: SDI Online
+//A trip to the corner store
+
+
+//Variables
+var cornerStore = 'Walgreens' 
+    ride = 'mazda 3'
+    firstThought = 'I should buy a bentley'
+    secondThought = 'I should buy a maserati'
+    sheSpeaks = 'What is up with this light!'
+    firstLight = '1'
+    secondLight = '2'
+    time = 'twenty minutes'
+    storeOpen = true
+    
+    
+    
+    
+//Print  
+    console.log('I woke up desperate for cereal.');
+    console.log('I had to think fast,I had no milk.');
+    console.log('I started up my ',ride,' and headed out.');
+    console.log('I was on my way to ',cornerStore,' when I stumbled across light ',firstLight);
+    console.log('As I waited at the first light, I said to myself, ',firstThought, '?'); 
+    console.log('I continued on with my trip to reach light ',secondLight,' and said to myself, ',secondThought,);
+    console.log('I waited at the light until I had run out of patience and said "',sheSpeaks,'"' '!' );
+    console.log('I finally reached ',cornerStore,' after ',time,' and it was closed.');
+    
+//Script
+
+ 	if ( 'storeOpen == true')
+ 	
+
+
+    
+    
+    
